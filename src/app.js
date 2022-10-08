638c65f96d3acb75a8fe60ab41d20d33a58a0b08
AmandaE19 <amanda.emidio.557@gmail.com>
2022-10-08 15:52:16 -0400

    code updated

--- src/app.js
+++ src/app.js
@@ -2,7 +2,22 @@ const express = require('express');
 const app = express()
 const cors = require("cors");
 const admin = require("firebase-admin");
-const serviceAccount = require("./credentials.json");
+require('dotenv').config()
+// const serviceAccount = require("./credentials.json");
+
+serviceAccount = {
+    "type": "service_account",
+    "project_id": process.env.PROJECT_ID,
+    "private_key_id": process.env.PRIVATE_KEY_ID,
+    "private_key": process.env.PRIVATE_KEY,
+    "client_email": process.env.CLIENT_EMAIL,
+    "client_id": process.env.CLIENT_ID,
+    "auth_uri": process.env.AUTH_URI,
+    "token_uri": process.env.TOKEN_URI,
+    "auth_provider_x509_cert_url": process.env.AUTH_PROVIDER,
+    "client_x509_cert_url": process.env.CLIENT_URL
+  }
+  
 
 admin.initializeApp({
     credential: admin.credential.cert(serviceAccount)
